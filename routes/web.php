c0f7d671b0af51a4167685e958cf3d76c42092b9
StarVinn <dementriadelvin22@gmail.com>
2025-10-20 10:46:37 +0700

    fix: not found controller route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AddbookController;
-use App\Http\Controllers\BooklistController;
+use App\Http\Controllers\web\Master\Book\BooklistController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\web\Master\DashboardController;
 use App\Http\Controllers\web\Master\Auth\AuthController;
@@ -16,6 +16,6 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::get('/', [DashboardController::class, 'showDashboard'])->name('dashboard');
     Route::get('/booklist', [BooklistController::class, 'index'])->name('booklist');
-    Route::get('/addbook', [AddbookController::class, 'index'])->name('addbook');
+    // Route::get('/addbook', [AddbookController::class, 'index'])->name('addbook');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
