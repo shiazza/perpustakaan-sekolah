1a579779cad45bdd34baac03e4900d382b307a82
StarVinn <dementriadelvin22@gmail.com>
2025-12-05 10:11:00 +0700

    fix: middleware error an*

--- routes/api.php
+++ routes/api.php
@@ -15,17 +15,17 @@ Route::get('/user', function (Request $request) {
 // Auth routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/logout', [AuthController::class, 'logout']);
 
 // Home routes
 Route::get('/home', [HomeController::class, 'index']);
 Route::get('/home/{id}', [HomeController::class, 'show']);
 
 // Borrow routes
-Route::post('/borrow/{bc_id}', [BorrowController::class, 'borrowBook'])->middleware('auth:sanctum');
-Route::post('/return/{borrow_id}', [BorrowController::class, 'returnBook'])->middleware('auth:sanctum');
+Route::post('/borrow/{bc_id}', [BorrowController::class, 'borrowBook']);
+Route::post('/return/{borrow_id}', [BorrowController::class, 'returnBook']);
 
 // Wishlist routes
-Route::post('/wishlist/add/{bc_id}', [WishlistController::class, 'addToWishlist'])->middleware('auth:sanctum');
-Route::delete('/wishlist/remove/{bc_id}', [WishlistController::class, 'removeFromWishlist'])->middleware('auth:sanctum');
-Route::get('/wishlist', [WishlistController::class, 'getWishlist'])->middleware('auth:sanctum');
+Route::post('/wishlist/add/{bc_id}', [WishlistController::class, 'addToWishlist']);
+Route::delete('/wishlist/remove/{bc_id}', [WishlistController::class, 'removeFromWishlist']);
+Route::get('/wishlist', [WishlistController::class, 'getWishlist']);
